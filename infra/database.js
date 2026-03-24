e1bd798257e7392f679e6280d79588c260dda684
Leonardo Sbarai <leonardo.sbarai@gmail.com>
2026-03-24 14:13:42 -0300

    fix eslint linting

--- infra/database.js
+++ infra/database.js
@@ -21,17 +21,19 @@ async function getNewClient() {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
+    ssl: getSSLValues(),
   });
 
   await client.connect();
   return client;
 }
 
-export default {
+const database = {
   query,
   getNewClient,
 };
+
+export default database;
 
 function getSSLValues() {
   if (process.env.POSTGRES_CA) {
